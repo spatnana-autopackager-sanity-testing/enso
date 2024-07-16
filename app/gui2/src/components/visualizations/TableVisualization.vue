9a7434f58be90ac78f8b622460406023ac6766e4
Martha King <martha_king@hotmail.co.uk>
2024-07-16 17:27:40 +0100

    very basic new node component

--- app/gui2/src/components/visualizations/TableVisualization.vue
+++ app/gui2/src/components/visualizations/TableVisualization.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
 import icons from '@/assets/icons.svg'
+import CreateNewNode from '@/components/CreateNewNode.vue'
 import {
   clipboardNodeData,
   tsvTableToEnsoExpression,
@@ -749,7 +750,7 @@ onUnmounted(() => {
     <template #toolbar>
       <TextFormattingSelector @changeFormat="(i) => updateTextFormat(i)" />
       <div v-if="isCreateNodeVisible">
-        <button>CREATE COMPONENT</button>
+        <CreateNewNode />
       </div>
     </template>
     <div ref="rootNode" class="TableVisualization" @wheel.stop @pointerdown.stop>
